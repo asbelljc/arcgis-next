37bbba398d487130d232bf803ce2f280d57b2475
Jonathan Asbell <asbelljc@gmail.com>
2022-08-26 17:50:38 -0400

    Added Home stylings and plants feature component.

---
+++ features/plants/plants.tsx
@@ -0,0 +1,64 @@
+import styles from '../../styles/Home.module.css';
+import useSWR from 'swr';
+import { useState, useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { updateTypes } from './plantsSlice';
+import Loader from '../../components/loader';
+import { useRouter } from 'next/router';
+
+const fetcher = async (
+  input: RequestInfo,
+  init: RequestInit,
+  ...args: any[]
+) => {
+  const res = await fetch(input, init);
+  return res.json();
+};
+
+const Plants = () => {
+  const { data, error } = useSWR('/api/powerplants', fetcher);
+
+  const types = useAppSelector((state) => state.plants.types);
+  const dispatch = useAppDispatch();
+
+  const [isLoading, setLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (data) {
+      dispatch(updateTypes(data.types));
+      setLoading(false);
+    }
+  }, [data, error, dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className={styles.loader}>
+        <Loader />
+      </div>
+    );
+  }
+
+  if (!types.length) {
+    return <p>No data</p>;
+  }
+
+  return (
+    <ul className={styles.list}>
+      {types.map((value, idx) => (
+        <li
+          className={styles.listItem}
+          key={`${value}-${idx}`}
+          onClick={() => router.push(`/webmap?type=${value}`)}
+        >
+          {value}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Plants;
---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import '../styles/globals.css';
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from '../app/store';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+  );
+}
+
+export default MyApp;
