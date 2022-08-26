5332a9ff7a0f9ab6d893bcac13643090997fa75d
Jonathan Asbell <asbelljc@gmail.com>
2022-08-26 16:39:26 -0400

    Started next app, added TS support, and made an initial api route.

---
+++ pages/api/powerplants.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { executeQueryJSON } from '@arcgis/core/rest/query';
+
+const PLANT_URL = '...';
+
+type Data = {
+  plantTypes: string[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data> // generic type; accepts our specified Data type
+) {
+  const query = {
+    outFields: ['fuel1'],
+    where: '1=1',
+    returnDistinctValues: true,
+    returnGeometry: false,
+  };
+
+  const results = await executeQueryJSON(PLANT_URL, query);
+  const values = results.features
+    .map((feature) => feature.attributes['fuel1'])
+    .filter(Boolean) // this trick passes each item to the Boolean() object, coercing truthy/falsy values to true/false (thus eliminating nulls and undefineds)
+    .sort();
+
+  res.status(200).json({ plantTypes: values });
+}
